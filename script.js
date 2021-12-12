1be00690d20856a1dc71562c266a6c14659912ba
oliver7011 <oliver.marsh7011@gmail.com>
2021-12-12 01:50:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,158 @@
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 380,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#6F64EA",
+      "opacity": 0.4,
+      "width": 0.5
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": false
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+function swap(){
+
+
+
+  if (document.getElementById("flip-card-inner").style.transform == 'rotateY(180deg)') {
+      document.getElementById("flip-card-inner").style.transform = 'rotateY(0deg)';
+
+  } else {
+      document.getElementById("flip-card-inner").style.transform = 'rotateY(180deg)';
+
+  }
+  var b1 = document.getElementById("button1");
+  var b1Color = window.getComputedStyle(b1, null).getPropertyValue("background-color");
+  var b1ForColor = window.getComputedStyle(b1, null).getPropertyValue("color");
+  var b2 = document.getElementById("button2");
+  var b2Color = window.getComputedStyle(b2, null).getPropertyValue("background-color");
+  var b2ForColor = window.getComputedStyle(b2, null).getPropertyValue("color");
+  document.getElementById("button1").style.backgroundColor = b2Color;
+  document.getElementById("button2").style.backgroundColor = b1Color;
+
+  document.getElementById("button1").style.color = b2ForColor;
+  document.getElementById("button2").style.color = b1ForColor;
+
+
+}
